c3632f389f583972c4a7f05018f699ae30b188b9
Nisha Gowda <nisha.gowda@utexas.edu>
2021-01-14 00:55:50 -0600

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("you must be on production")
